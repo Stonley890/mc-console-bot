b4dbed51139debe712aaf1f880a497049c6632c8
Stonley890 <79172597+Stonley890@users.noreply.github.com>
2022-12-01 13:34:28 -0800

    Removed status channel

--- console-bot.js
+++ console-bot.js
@@ -14,9 +14,6 @@ var MC_SERVER_START_SCRIPT = "C:/Users/username/start.bat";
 // The channel ID of your game chat channel
 var gamechat = "<ID_HERE_WITHOUT_BRACKETS>"
 
-// The channel ID of your status voice channel
-var serverstatus = "<ID_HERE_WITHOUT_BRACKETS>"
-
 // The name of the role allowed to start, kill, and send commands to the server
 var consolemaster = "<NAME_HERE_WITHOUT_BRACKETS>"
 
@@ -59,14 +56,8 @@ bot.on("messageCreate", function (message) {
 
                     // Send message to Discord
                     bot.channels.cache.get(gamechat).send(data.toString().split("INFO]: ")[1]);
-
                 }
-
             });
-
-            message.guild.channels.resolve(serverstatus).setName('SERVER: UP ✅');
-
-
 
             mcserver.on('close', function (code) {
                 console.log("child process exited with code " + code);
@@ -96,10 +87,8 @@ bot.on("messageCreate", function (message) {
 
             // Stop the server
             kill(mcserver.pid);
-            message.guild.channels.resolve(serverstatus).setName('SERVER: DOWN ❌');
             mcserver = null;
         }
-        message.guild.channels.resolve(serverstatus).setName('SERVER: DOWN ❌');
         mcserver = null;
 
     } else if (message.content.startsWith("-") && message.member.roles.cache.some(role => role.name === consolemaster) && (mcserver != null)) {
